49967f3200414ae30386e2cac322d4a003311a50
Campo Elias Pinillos <1207@holbertonschool.com>
2020-06-13 23:49:56 -0500

    [Update] Function

--- Automate_borring_stuff/letter_count.py
+++ Automate_borring_stuff/letter_count.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+'''This program counts the letters in a message pass to it'''
 import pprint
+import sys
 
-message = 'It was a bright cold day in April, and the clock were striking thirteen.'
+# Prueba 'It was a bright cold day in April, and the clock were striking thirteen.'
+input = sys.argv[1]
+message = "".join(input.split()) # Removing spaces
 count = {}
 
 for character in message.upper():
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-pprint.pprint(count)
-
+# pprint.pprint(count) # Pretty
 rjtex = pprint.pformat(count)
 print(rjtex)
