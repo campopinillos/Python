7efd309d51733cd81219abeb5fdcd6a9d009f5f4
Campo Elias Pinillos <1207@holbertonschool.com>
2020-07-06 23:16:24 -0500

    [Update] Cases

--- Automate_borring_stuff/regex.py
+++ Automate_borring_stuff/regex.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
-"""Find a phone number using regular expression"""
+"""
+Find a phone number using regular expression
+Methods: seach, findall, group, groups
+Symbols:
+The ? matches zero or one of the preceding group.
+The * matches zero or more of the preceding group.
+The + matches one or more of the preceding group.
+The {n} matches exactly n of the preceding group.
+The {n,} matches n or more of the preceding group.
+The {,m} matches 0 to m of the preceding group.
+The {n,m} matches at least n and at most m of the preceding group.
+{n,m}? or *? or +? performs a nongreedy match of the preceding group.
+^spam means the string must begin with spam.
+spam$ means the string must end with spam.
+The . matches any character, except newline characters.
+\d, \w, and \s match a digit, word, or space character, respectively.
+\D, \W, and \S match anything except a digit, word, or space character, respectively.
+[abc] matches any character between the brackets (such as a, b, or c).
+[^abc] matches any character that isn’t between the brackets.
+"""
 
 import re
 
@@ -112,3 +131,23 @@ nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
 mo = nameRegex.search('First Name: Alan23144 Last Name: Sweigart2132')
 print(mo.group(1))
 print(mo.group(2))
+
+# Matching Newlines with the Dot Character
+noNewlineRegex = re.compile('.*')
+print(noNewlineRegex.search('Serve the public trust.\nProtect the innocent. \nUphold the law.').group())
+newlineRegex = re.compile('.*', re.DOTALL)
+print(newlineRegex.search('Serve the public trust.\nProtect the innocent. \nUphold the law.').group())
+
+# Case-insensitive matching
+robocop = re.compile(r'robocop', re.I) # re.IGNORECASE for non case sensitive
+print(robocop.search('RoboCop is part man, part machine, all cop.').group())
+
+"""Substituting Strings with the sub() method"""
+message = 'Agent Alice gave the secret documents to Agent Bob.'
+namesRegex = re.compile(r'Agent \w+')
+print(namesRegex.sub('CENSORED', message))
+
+# Use \num backslash number to replace by group
+message = 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.'
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+print(agentNamesRegex.sub(r'Agent \1*****', message))
